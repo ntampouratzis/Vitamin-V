876f7f3ad65401730d5350c8c5be75b3f6ad9911
ntampouratzis <32739524+ntampouratzis@users.noreply.github.com>
2025-03-26 20:44:26 +0200

    Update spmv_m8.c

--- benchmarks/riscv-vectorized-benchmark-suite/_spmv/src/spmv_m8.c
+++ benchmarks/riscv-vectorized-benchmark-suite/_spmv/src/spmv_m8.c
@@ -21,12 +21,6 @@
 
 void spmv_intrinsics(const size_t nrows, double *a, uint64_t *ia, uint64_t *ja, double *x, double *y) {
 
-    _MMR_f64 va;
-    _MMR_u64 v_idx_row;
-    _MMR_f64 vx;
-    _MMR_f64 vprod;
-    _MMR_f64 part_res;
-
     for (size_t row = 0; row < nrows; row++) {
         int64_t nnz_row = ia[row + 1] - ia[row];
         int64_t idx     = ia[row];
@@ -58,17 +52,9 @@ void spmv_intrinsics(const size_t nrows, double *a, uint64_t *ia, uint64_t *ja, 
                 // Extract the scalar result from the accumulated vector
                 sum += __riscv_vfmv_f_s_f64m1_f64(ret_sum);
 
-                //va        = _MM_LOAD_f64(&a[idx+colid],  gvl);
-                //v_idx_row = _MM_LOAD_u64(&ja[idx+colid], gvl);
-                //v_idx_row = _MM_SLL_VX_u64(v_idx_row, 3, gvl);
-                //vx        = _MM_LOAD_INDEX_f64(x, v_idx_row, gvl);
-                //vprod     = _MM_MACC_f64(vprod,va, vx, gvl);
             }
 
             y[row] = sum;
-//             gvl       = _MMR_VSETVL_E64M1(nnz_row);
-//             part_res  = _MM_REDSUM_f64(vprod, part_res, gvl);
-//             y[row]    = _MM_VGETFIRST_f64(part_res);
         }
     }
 }
