a9e7e496b6acdc2e5e067a2804a6546107854620
ntampouratzis <32739524+ntampouratzis@users.noreply.github.com>
2025-04-14 12:56:31 +0300

    Update ComputeDotProduct_ref.cpp

--- benchmarks/HPCG_rvv/src/ComputeDotProduct_ref.cpp
+++ benchmarks/HPCG_rvv/src/ComputeDotProduct_ref.cpp
@@ -8,6 +8,7 @@
 // Jack Dongarra     (dongarra@eecs.utk.edu)
 // Piotr Luszczek    (luszczek@eecs.utk.edu)
 //
+// RVV: Nikolaos Tampouratzis (ntampouratzis@ihu.gr)
 // ***************************************************
 //@HEADER
 
